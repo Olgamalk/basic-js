6c33523ad4aafc06d7796d26bbcb7d56e6f548cf
Olga Malkovich <43276405+Olgamalk@users.noreply.github.com>
2021-03-18 14:11:45 +0300

    fix: update solution

--- src/what-season.js
+++ src/what-season.js
@@ -5,6 +5,7 @@ module.exports = function getSeason(date) {
     return `Unable to determine the time of year!`;
   };
   let month = date.getMonth();
+  if (!date instanceof Date || date.hasOwnProperty('getMonth')) throw new Error ('Invalid input');
   if (month >= 2 && month <= 4) {
     return 'spring';
   } else if (month >= 5 && month <= 7) {
@@ -14,6 +15,4 @@ module.exports = function getSeason(date) {
   } else if (month === 11 || month === 0 || month === 1)  {
     return "winter";
   };
-   
-
 };
