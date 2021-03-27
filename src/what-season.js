adcfae41324e2f0d406b2fdf0edabfa65ff9eb98
Olga Malkovich <olga.malkovich@gmail.com>
2021-03-27 20:23:11 +0300

    fix: add solution 'what seasons'

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!date) {
+    return `Unable to determine the time of year!`;
+  };
+  let month = date.getMonth();
+  if (!date instanceof Date || date.hasOwnProperty('getMonth')) throw new Error ('Invalid input');
+  if (month >= 2 && month <= 4) {
+    return 'spring';
+  } else if (month >= 5 && month <= 7) {
+    return 'summer';
+  } else if (month >= 8 && month <= 10) {
+    return 'autumn';
+  } else if (month === 11 || month === 0 || month === 1)  {
+    return "winter";
+  };
 };
