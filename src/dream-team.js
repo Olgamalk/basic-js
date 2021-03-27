6b7b15bc14057581bac7beae2c9f945af3946f33
Olga Malkovich <olga.malkovich@gmail.com>
2021-03-27 21:10:36 +0300

    fix: add solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let team = [];
+  for (letter of members) {
+    if (typeof(letter) === "string")
+    team.push(letter.trim()[0].toUpperCase());
+  }
+  return team.sort().join('');
 };
