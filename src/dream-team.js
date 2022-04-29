9d9f3c686581d628ae5ac390c9b7aea3ab0472c7
Olga Malkovich <olga.malkovich@gmail.com>
2022-04-29 16:23:34 +0300

    feat: add solution for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+ if (!Array.isArray(members)) {
+   return false;
+ } else {
+   let team = [];
+   let letter;
+   for (letter of members) {
+     if (typeof(letter) === 'string') {
+       team.push(letter.trim()[0].toUpperCase());
+     }
+   }
+  return team.sort().join('');
+ }
 }
 
 module.exports = {
