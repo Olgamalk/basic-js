e87770fb43058d7b363b424ff719855ecee268ea
Olga Malkovich <olga.malkovich@gmail.com>
2022-05-02 18:39:00 +0300

    feat: add solution for dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
+
+  if (!Array.isArray(domains)) {
+    return false;
+  }
+
+  const result = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    let key = '';
+    let array = domains[i].split('.').reverse();
+
+    for (let j = 0; j < array.length; j++) {
+      key = key + '.' + array[j];
+      if (Object.keys(result).includes(key)) {
+        result[key] = result[key] + 1;
+      } else {
+        result[key] = 1;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
